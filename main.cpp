48da80ddd17c4ee69c6ca53bb6a731810ebfbaec
eddyEMA <eddygharbiema@gmail.com>
2015-06-15 15:00:41 +0200

    read file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
+#include "iostream"
+using namespace std;
+
+int grille[9][9];
 int main(int argc, char *argv[])
 {
+    int i,j;
+
+    for(int i=0;i<9;i++)
+    {
+        for(int j=0;j<9;j++)
+        {
+            cin >> grille[i][j];
+        }
+    }
+    for(int i=0;i<9;i++)
+    {
+        for(int j=0;j<9;j++)
+        {
+            cout << grille[i][j] << ' ';
+        }
+        cout << endl;
+    }
     return 1;
 }
